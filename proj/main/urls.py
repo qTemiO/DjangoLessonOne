71f711d5cb39bba377d2612f6fd1abaa384e6a5b
qTemiO <71713499+qTemiO@users.noreply.github.com>
2021-01-28 21:00:11 +0300

    last changes
    с django закончил, настало время restframework

--- proj/main/urls.py
+++ proj/main/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from . import  views
+from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('data', views.getData),
-    path('dataRender', views.getDataRender)
+    path('data/', views.getData),
+    path('dataRender/', views.getDataRender),
+    path('dataRender/<int:pk>/', views.getDataPk),
+    path('dataVue/', views.getDataVue),
+    path('dataVue/sendNames/', views.sendNames),
+    path('dataVue/sendAll/', views.sendAll),
+    path('dataVue/namesFilter/', views.namesFilter),
 ]
--- proj/main/views.py
+++ proj/main/views.py
@@ -1,4 +1,8 @@
+import json
+
+from loguru import logger
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 
 from .models import Data
@@ -12,9 +16,54 @@ def getData(request):
         'Data': [i.toDict() for i in Data.objects.all()]
         })
 
+def getDataPk(request, pk):
+    return JsonResponse({
+        'Data': Data.objects.get(pk=pk).toDict()
+        })
+
 def getDataRender(request):
     datas = Data.objects.all()
     context = {
         'datas': datas,
     }
     return render(request, 'main/render.html', context)
+
+def getDataVue(request):
+    return render(request, 'main/vue.html')
+
+@csrf_exempt
+def sendNames(request):
+    datas = Data.objects.all()
+    return JsonResponse({
+        'name': [data.Name for data in datas]
+    }, safe=False)
+
+    
+@csrf_exempt
+def sendAll(request):
+    return JsonResponse({
+        'Data': [i.toDict() for i in Data.objects.all()]
+    }, safe=False)
+
+@csrf_exempt
+def namesFilter(request):
+    params = json.loads(request.body.decode("utf-8"))
+    textFilter = params['textFilter']
+    filterCategory = params['filterCategory']
+
+    logger.debug(textFilter)
+    logger.debug(filterCategory)
+    data = []
+
+    if (filterCategory == "Категория"):
+        logger.debug("Category!")
+        data = Data.objects.filter(Category=textFilter)
+    if (filterCategory == "Текст"):
+        logger.debug("Text!")
+        data = Data.objects.filter(Text=textFilter)
+    if (filterCategory == "Имя"):
+        logger.debug("Name!")
+        data = Data.objects.filter(Name=textFilter)
+    return JsonResponse({
+        'Data': [i.toDict() for i in data],
+        }, safe=False)
